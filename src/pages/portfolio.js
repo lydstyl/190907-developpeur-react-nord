1d2ac8edbba97cbf81e7ebf434abc337040669dc
Gabriel Brun <lydstyl@gmail.com>
2019-09-08 14:10:54 +0200

    Porfolio list of creations ok

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,16 +1,44 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import PostLink from "../components/post-link"
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Portfolio" />
-    <h2>Portfolio</h2>
-    <p>Welcome to my portfolio</p>
-    <Link to="/">Retourner au CV</Link>
-  </Layout>
-)
+const Portfolio = ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) => {
+  const Posts = edges
+    .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+    .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
 
-export default SecondPage
+  return (
+    <Layout>
+      <SEO title="Portfolio" />
+      <h2>Portfolio</h2>
+      <p>Welcome to my portfolio</p>
+      {Posts}
+    </Layout>
+  )
+}
+
+export default Portfolio
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`

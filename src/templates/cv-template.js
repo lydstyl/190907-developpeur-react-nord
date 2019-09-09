c53bf06ddda7ba2231b0326b387228f9ed969407
Gabriel Brun <lydstyl@gmail.com>
2019-09-09 19:30:24 +0200

    creation field order

--- src/templates/cv-template.js
+++ src/templates/cv-template.js
@@ -67,7 +67,7 @@ export default function Template({
       <div className="cv-description-box shadow p-3 mt-5 bg-white rounded">
         <img
           src="https://media.licdn.com/dms/image/C4E03AQHnYrk8fffMFw/profile-displayphoto-shrink_200_200/0?e=1573689600&v=beta&t=QsruEIIjPag1WnfHE27ejIJ8-No84k7FWz6HJjuX9gg"
-          alt="photo Gabriel BRUN"
+          alt="Gabriel BRUN"
         />
         <p className="text-justify">{description}</p>
         <p className="text-right font-italic">Mise à jour le {date}</p>

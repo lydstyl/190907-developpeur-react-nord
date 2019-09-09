a83d29e64972b0a8677a04cf54f06eee43214308
Gabriel Brun <lydstyl@gmail.com>
2019-09-09 19:26:11 +0200

    Gabriel's picture

--- src/templates/cv-template.js
+++ src/templates/cv-template.js
@@ -64,9 +64,13 @@ export default function Template({
   return (
     <Layout showPortfolioLink="true">
       <SEO title={title} />
-      <div className="shadow p-3 mt-5 bg-white rounded">
-        <p className="text-right font-italic">Mise à jour le {date}</p>
+      <div className="cv-description-box shadow p-3 mt-5 bg-white rounded">
+        <img
+          src="https://media.licdn.com/dms/image/C4E03AQHnYrk8fffMFw/profile-displayphoto-shrink_200_200/0?e=1573689600&v=beta&t=QsruEIIjPag1WnfHE27ejIJ8-No84k7FWz6HJjuX9gg"
+          alt="photo Gabriel BRUN"
+        />
         <p className="text-justify">{description}</p>
+        <p className="text-right font-italic">Mise à jour le {date}</p>
       </div>
       <h2 className="shadow p-3 mt-5 bg-white rounded">
         <i className="fas fa-bullseye"></i> Objectif:{" "}

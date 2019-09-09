7d9fdefcd2d4bc83ec0f52001b16a13969c23c58
Gabriel Brun <lydstyl@gmail.com>
2019-09-09 17:21:53 +0200

    css date creations

--- src/components/post-link.js
+++ src/components/post-link.js
@@ -4,7 +4,8 @@ import { Link } from "gatsby"
 const PostLink = ({ post }) => (
   <li>
     <Link to={post.frontmatter.path} className="btn btn-secondary btn-sm m-2">
-      {post.frontmatter.title} ({post.frontmatter.date})
+      {post.frontmatter.title}{" "}
+      <span className="text-warning">débuté le {post.frontmatter.date}</span>
     </Link>
   </li>
 )

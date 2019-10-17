e5a4d03db254b5d09744f5390fc6d13fb8841154
Gabriel Brun <lydstyl@gmail.com>
2019-10-17 17:12:24 +0200

    ReviewWebSite content

--- src/components/ReviewWebSite/ReviewWebSite.jsx
+++ src/components/ReviewWebSite/ReviewWebSite.jsx
@@ -46,32 +46,30 @@ const ReviewWebSite = () => {
       </p>
 
       <h3>Pourquoi passer par moi</h3>
-      <p>
-        <ul>
-          <li>
-            Que ce soit un site Internet pour vous faire connaitre, convertir
-            des prospects, trouver des clients ou une Web App qui pourra être
-            par exemple un outil qui vous fera gagner du temps, je vous
-            proposerai la meilleure solution entièrement personnalisée pour
-            répondre au mieux à votre besoin.
-          </li>
-          <li>
-            J'utilise des technologies à la pointe vous permettant d'avoir un
-            site très rapide. Par exemple pour un site de type blog, il sera
-            plus rapide que n'importe quel site fait avec Wordpress. Ainsi, le
-            référencement naturel et l'experience du visiteur seront améliorés.
-          </li>
-          <li>
-            Avec la technologie Headless CMS:
-            <ul>
-              <li>Vous pouvez choisir votre CMS.</li>
-              <li>La performance est au maximum possible.</li>
-              <li>La sécurité est au maximum possible.</li>
-              <li>Votre site sera extremement scalable.</li>
-            </ul>
-          </li>
-        </ul>
-      </p>
+      <ul>
+        <li>
+          Que ce soit un site Internet pour vous faire connaitre, convertir des
+          prospects, trouver des clients ou une Web App qui pourra être par
+          exemple un outil qui vous fera gagner du temps, je vous proposerai la
+          meilleure solution entièrement personnalisée pour répondre au mieux à
+          votre besoin.
+        </li>
+        <li>
+          J'utilise des technologies à la pointe vous permettant d'avoir un site
+          très rapide. Par exemple pour un site de type blog, il sera plus
+          rapide que n'importe quel site fait avec Wordpress. Ainsi, le
+          référencement naturel et l'experience du visiteur seront améliorés.
+        </li>
+        <li>
+          Avec la technologie Headless CMS:
+          <ul>
+            <li>Vous pouvez choisir votre CMS.</li>
+            <li>La performance est au maximum possible.</li>
+            <li>La sécurité est au maximum possible.</li>
+            <li>Votre site sera extremement scalable.</li>
+          </ul>
+        </li>
+      </ul>
     </ShadowBoxWrapper>
   )
 }

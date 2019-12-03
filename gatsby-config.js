b3bb92063a94403f08b10b560829f18fd0256c4e
Gabriel Brun <lydstyl@gmail.com>
2019-12-03 12:35:22 +0100

    clear config gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,13 +17,6 @@ module.exports = {
     {
       resolve: `gatsby-plugin-sitemap`,
       options: {
-        // output: `/some-other-sitemap.xml`,
-
-        // Exclude specific pages or groups of pages using glob parameters
-        // See: https://github.com/isaacs/minimatch
-        // The example below will exclude the single `path/to/page` and all routes beginning with `category`
-
-        // exclude: ["/category/*", `/path/to/page`],
         query: `
           {
             site {
@@ -41,54 +34,51 @@ module.exports = {
             }
         }`,
         serialize: ({ site, allSitePage }) =>
-          allSitePage.edges
-            // .filter(edge => edge.node.path !== "/agenda/")
-            .map(edge => {
-              if (edge.node.path.includes("/services")) {
-                return {
-                  url: site.siteMetadata.siteUrl + edge.node.path,
-                  changefreq: `daily`,
-                  priority: 0.9,
-                }
+          allSitePage.edges.map(edge => {
+            if (edge.node.path.includes("/services")) {
+              return {
+                url: site.siteMetadata.siteUrl + edge.node.path,
+                changefreq: `daily`,
+                priority: 0.9,
               }
+            }
 
-              if (edge.node.path.includes("/cv")) {
-                return {
-                  url: site.siteMetadata.siteUrl + edge.node.path,
-                  changefreq: `weekly`,
-                  priority: 0.7,
-                }
+            if (edge.node.path.includes("/cv")) {
+              return {
+                url: site.siteMetadata.siteUrl + edge.node.path,
+                changefreq: `weekly`,
+                priority: 0.7,
               }
+            }
 
-              // portfolio
-              if (edge.node.path === "/") {
-                return {
-                  url: site.siteMetadata.siteUrl + edge.node.path,
-                  changefreq: `weekly`,
-                  priority: 0.6,
-                }
+            // portfolio
+            if (edge.node.path === "/") {
+              return {
+                url: site.siteMetadata.siteUrl + edge.node.path,
+                changefreq: `weekly`,
+                priority: 0.6,
               }
-              if (edge.node.path.includes("/portfolio/")) {
-                return {
-                  url: site.siteMetadata.siteUrl + edge.node.path,
-                  changefreq: `monthly`,
-                  priority: 0.5,
-                }
-              }
-
-              // other pages
+            }
+            if (edge.node.path.includes("/portfolio/")) {
               return {
                 url: site.siteMetadata.siteUrl + edge.node.path,
                 changefreq: `monthly`,
-                priority: 0.1,
+                priority: 0.5,
               }
-            }),
+            }
+
+            // other pages
+            return {
+              url: site.siteMetadata.siteUrl + edge.node.path,
+              changefreq: `monthly`,
+              priority: 0.1,
+            }
+          }),
       },
     },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -119,21 +109,27 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-
-    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
         trackingId: "UA-150201460-1",
+      },
+    },
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 1078,
+            },
+          },
+        ],
       },
     },
   ],

569e8d68ad82268f040c4559da8bff88acf6c998
Gabriel Brun <lydstyl@gmail.com>
2020-01-30 16:52:13 +0100

    a:hover text-decoration: none

--- src/components/Experiences/style.js
+++ src/components/Experiences/style.js
@@ -3,6 +3,10 @@ import styled from "styled-components"
 const DownLoadCVBox = styled.div`
   display: flex;
   justify-content: space-evenly;
+
+  a:hover {
+    text-decoration: none;
+  }
 `
 
 export default DownLoadCVBox

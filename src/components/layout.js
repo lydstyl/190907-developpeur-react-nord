0371f67f0eb213b1feea9d2d03f49f791ea6bf6c
Gabriel Brun <lydstyl@gmail.com>
2020-02-01 11:00:35 +0100

    scaleRotate Menu effect working

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import { scaleRotate as Menu } from "react-burger-menu"
 
 import Header from "./Header/Header"
 import "./layout.css"
@@ -23,39 +24,66 @@ const Layout = ({ children }) => {
     }
   `)
 
-  return (
-    <div className="container">
-      <link
-        rel="stylesheet"
-        href="https://use.fontawesome.com/releases/v5.4.2/css/all.css"
-      />
+  function showSettings(event) {
+    event.preventDefault()
+    // .
+    // .
+    // .
+  }
 
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <main>{children}</main>
-      <footer className="shadow p-3 mt-5 mb-5 bg-white rounded">
-        <div className="row">
-          <div className="col-md">
-            <a className="text-decoration-none" href="tel:+33781154503">
-              <i className="fas fa-mobile-alt"></i> 07 81 15 45 03
-            </a>
+  return (
+    <div id="outer-container">
+      <Menu pageWrapId={"page-wrap"} outerContainerId={"outer-container"}>
+        <a id="home" className="menu-item" href="/">
+          Home
+        </a>
+        <a id="about" className="menu-item" href="/about">
+          About
+        </a>
+        <a id="contact" className="menu-item" href="/contact">
+          Contact
+        </a>
+        <a onClick={showSettings} className="menu-item--small" href="">
+          Settings
+        </a>
+      </Menu>
+
+      <main id="page-wrap" className="container">
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.4.2/css/all.css"
+        />
+
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <main>{children}</main>
+        <footer className="shadow p-3 mt-5 mb-5 bg-white rounded">
+          <div className="row">
+            <div className="col-md">
+              <a className="text-decoration-none" href="tel:+33781154503">
+                <i className="fas fa-mobile-alt"></i> 07 81 15 45 03
+              </a>
+            </div>
+            <div className="col-md">
+              <a
+                className="text-decoration-none"
+                href="mailto:lydstyl@gmail.com"
+              >
+                <i className="far fa-envelope"></i> lydstyl@gmail.com
+              </a>
+            </div>
+            <div className="col-md">
+              <a
+                className="text-decoration-none"
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/lydstyl"
+              >
+                <i className="fab fa-github"></i> Github
+              </a>
+            </div>
           </div>
-          <div className="col-md">
-            <a className="text-decoration-none" href="mailto:lydstyl@gmail.com">
-              <i className="far fa-envelope"></i> lydstyl@gmail.com
-            </a>
-          </div>
-          <div className="col-md">
-            <a
-              className="text-decoration-none"
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://github.com/lydstyl"
-            >
-              <i className="fab fa-github"></i> Github
-            </a>
-          </div>
-        </div>
-      </footer>
+        </footer>
+      </main>
     </div>
   )
 }

3d315f0cbd48d7ca1e5fa6381aa29de9f0498fc1
Gabriel Brun <lydstyl@gmail.com>
2020-05-21 17:00:57 +0200

    hide qrcode on mobile

--- src/components/Header/QRContactMe/QRContactMe.js
+++ src/components/Header/QRContactMe/QRContactMe.js
@@ -1,8 +1,17 @@
 import React from "react"
+import styled from "styled-components"
+
+const Img = styled.img`
+  display: none;
+
+  @media screen and (min-width: 768px) {
+    display: block;
+  }
+`
 
 export const QRContactMe = () => {
   return (
-    <img
+    <Img
       src="https://chart.googleapis.com/chart?cht=qr&chl=BEGIN%3AVCARD%0AVERSION%3A3.0%0AN%3ABrun%3BGabriel%0AFN%3AGabriel%20Brun%0AORG%3A%0ATITLE%3ADeveloppeur%20JS%20et%20React.js%0AADR%3A%3B%3B%3BRaismes%3B%3B59590%3BFrance%0ATEL%3BWORK%3BVOICE%3A%0ATEL%3BCELL%3A0781154503%0ATEL%3BFAX%3A%0AEMAIL%3BWORK%3BINTERNET%3Alydstyl%40gmail.com%0AURL%3Awww.developpeur-react-nord.com%0AEND%3AVCARD%0A&chs=180x180&choe=UTF-8&chld=L|2"
       alt="vcard contactez moi"
       title="VCARD, mes coordonnées sont aussi en bas de page."

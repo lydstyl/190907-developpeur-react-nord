61aaf13b825e8782a084347ab60121bee095c6f2
Gabriel Brun <lydstyl@gmail.com>
2020-05-27 16:23:04 +0200

    change animation to focus a call to action

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -16,27 +16,47 @@ const Header = ({ siteTitle }) => {
   }
 
   useEffect(() => {
-    const tl = gsap.timeline({ defaults: { duration: 0.5, ease: "elastic" } })
-    tl.from(".rounded", { rotation: 9 })
-      .from("h2", {
-        x: 50,
-        scale: 1.3,
+    const timeline = gsap.timeline({
+      defaults: { ease: "elastic" },
+    })
+    // timeline.from(".rounded", { rotation: 9 })
+    //   .from("h2", {
+    //     x: 50,
+    //     scale: 1.3,
 
-        ease: "slow(0.7, 0.7, false)",
-      })
+    //     ease: "slow(0.7, 0.7, false)",
+    //   })
 
-      .from("p", {
-        // ease: "expo.out",
-        ease: "bounce.out",
-        y: 20,
-      })
-      .from("span ", { x: -50, transformOrigin: "center bottom" })
-      .from("a", {
-        stagger: 0.2,
-        rotation: -20,
-        scale: 0.7,
-        delay: -0.1,
-      })
+    //   .from("p", {
+    //     // ease: "expo.out",
+    //     ease: "bounce.out",
+    //     y: 20,
+    //   })
+    //   .from("span ", { x: -50, transformOrigin: "center bottom" })
+
+    //   .from("a", {
+    //     stagger: 0.2,
+    //     rotation: -20,
+    //     scale: 0.7,
+    //     delay: -0.1,
+    //   })
+
+    const repeatEvery = 8000
+    const durationTime = repeatEvery / 4 / 1000
+
+    setInterval(() => {
+      timeline
+        .to(".menu-box a", { rotation: 180, duration: durationTime })
+        .to("footer .col-md:nth-child(2)", {
+          paddingLeft: 15,
+          ease: "bounce.out",
+        })
+        .to(".menu-box a", { rotation: 0, duration: durationTime })
+        .to("footer .col-md:nth-child(2)", {
+          paddingLeft: 50,
+          ease: "slow(0.7, 0.7, false)",
+        })
+    }, repeatEvery)
   }, [])
 
   return (

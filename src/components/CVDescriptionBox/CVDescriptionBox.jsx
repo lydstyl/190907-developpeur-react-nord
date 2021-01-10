4b820c23973b570b0f26340369881fe109259b13
Gabriel Brun <lydstyl@gmail.com>
2021-01-10 16:24:25 +0100

    fix warnings

--- src/components/CVDescriptionBox/CVDescriptionBox.jsx
+++ src/components/CVDescriptionBox/CVDescriptionBox.jsx
@@ -1,7 +1,7 @@
-import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
 
-import Image from "../Image/Image"
+import Image from '../Image/Image'
 
 const CVDescriptionBox = () => {
   const data = useStaticQuery(
@@ -20,25 +20,26 @@ const CVDescriptionBox = () => {
   const { date, description } = data.markdownRemark.frontmatter
 
   return (
-    <div className="cv-description-box shadow p-3 mt-5 bg-white rounded">
-      <h2 className="d-none">
+    <div className='cv-description-box shadow p-3 mt-5 bg-white rounded'>
+      <h2 className='d-none'>
         CV Gabriel Brun ou curriculum vitae développeur React.js
       </h2>
 
-      <Image src="photo_profil_gabriel_brun_github.jpg" />
+      <Image src='photo_profil_gabriel_brun_github.jpg' />
 
-      <p className="text-justify">{description}</p>
+      <p className='text-justify'>{description}</p>
 
-      <p className="text-right font-italic">Mise à jour le {date}</p>
+      <p className='text-right font-italic'>Mise à jour le {date}</p>
 
       <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube-nocookie.com/embed/u4LuxFWGtf0"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        title='cv vidéo'
+        width='560'
+        height='315'
+        src='https://www.youtube-nocookie.com/embed/u4LuxFWGtf0'
+        frameborder='0'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
         allowfullscreen
-      ></iframe>
+       />
     </div>
   )
 }
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,8 +1,8 @@
-import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import { Radar } from "react-chartjs-2"
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import { Radar } from 'react-chartjs-2'
 
-import SkillsBox from "./style"
+import SkillsBox from './style'
 
 const Skills = () => {
   const data = useStaticQuery(
@@ -33,111 +33,111 @@ const Skills = () => {
 
   const mainSkills = main
     .map(mainSkill => {
-      return mainSkill.title + ": " + mainSkill.rate + "/10"
+      return mainSkill.title + ': ' + mainSkill.rate + '/10'
     })
-    .join(", ")
+    .join(', ')
 
   const radarData = {
     labels: main.map(skill => skill.title),
     datasets: [
       {
         data: main.map(skill => skill.rate),
-        backgroundColor: "rgba(255, 193, 7, 0.3)", // #ffc107
-        borderColor: "rgba(255, 193, 7, 1)",
+        backgroundColor: 'rgba(255, 193, 7, 0.3)', // #ffc107
+        borderColor: 'rgba(255, 193, 7, 1)',
         borderWidth: 3,
-        label: "Actuelles",
-        lineTension: 0.2,
+        label: 'Actuelles',
+        lineTension: 0.2
       },
       {
         data: goal.map(skill => skill.rate),
-        backgroundColor: "rgba(255, 193, 7, 0.1)",
-        borderColor: "rgba(255, 193, 7, 1)",
+        backgroundColor: 'rgba(255, 193, 7, 0.1)',
+        borderColor: 'rgba(255, 193, 7, 1)',
         borderWidth: 3,
-        label: "Objectif",
+        label: 'Objectif',
         lineTension: 0.2,
-        borderDash: [5],
-      },
-    ],
+        borderDash: [5]
+      }
+    ]
   }
 
   const otherSkills = other
     .map(mainSkill => {
       return mainSkill.title
     })
-    .join(", ")
+    .join(', ')
 
   const options = {
     legend: {
-      position: "top",
+      position: 'top'
     },
     scale: {
       ticks: {
-        beginAtZero: true,
-      },
-    },
+        beginAtZero: true
+      }
+    }
   }
 
   return (
-    <SkillsBox className="skills shadow p-3 mt-5 bg-white rounded">
+    <SkillsBox className='skills shadow p-3 mt-5 bg-white rounded'>
       <h2>
-        <i className="far fa-check-square"></i> Compétences:
+        <i className='far fa-check-square' /> Compétences:
       </h2>
 
       <ul>
         <li>
           <h3>Principales:</h3>
 
-          <p className="mainSkills text-justify">{mainSkills}</p>
+          <p className='mainSkills text-justify'>{mainSkills}</p>
 
-          <div className="radar">
+          <div className='radar'>
             <Radar data={radarData} options={options} />
           </div>
         </li>
 
         <li>
           <h3>Autres:</h3>
-          <p className="text-justify">{otherSkills}</p>
+          <p className='text-justify'>{otherSkills}</p>
         </li>
 
         <li>
           <h3>Certificats:</h3>
 
-          <div className="certificates">
-            <p className="text-justify">
+          <div className='certificates'>
+            <p className='text-justify'>
               <a
-                target="_blank"
-                rel="noreferrer"
-                href="https://www.udemy.com/certificate/UC-2CPLK9HX/"
+                target='_blank'
+                rel='noopener noreferrer'
+                href='https://www.udemy.com/certificate/UC-2CPLK9HX/'
               >
                 React Front To Back
               </a>
             </p>
 
-            <p className="text-justify">
+            <p className='text-justify'>
               <a
-                target="_blank"
-                rel="noreferrer"
-                href="/assets/CodinGame-Gabriel_BRUN.pdf"
+                target='_blank'
+                rel='noopener noreferrer'
+                href='/assets/CodinGame-Gabriel_BRUN.pdf'
               >
                 Test Codin Game
               </a>
             </p>
 
-            <p className="text-justify">
+            <p className='text-justify'>
               <a
-                target="_blank"
-                rel="noreferrer"
-                href="https://skillvalue.com/en/certificate/ZZPZSS18ShQp4BM8FprEW8zj9KqJVIGpGsIDonbYmi8pRqFhY14c3KmTZUeV"
+                target='_blank'
+                rel='noopener noreferrer'
+                href='https://skillvalue.com/en/certificate/ZZPZSS18ShQp4BM8FprEW8zj9KqJVIGpGsIDonbYmi8pRqFhY14c3KmTZUeV'
               >
                 Certificat Skill Value
               </a>
             </p>
 
-            <p className="text-justify">
+            <p className='text-justify'>
               <a
-                target="_blank"
-                rel="noreferrer"
-                href="https://ude.my/UC-d5bbdc9f-62a2-4fe3-98ed-a8ac642f0880"
+                target='_blank'
+                rel='noopener noreferrer'
+                href='https://ude.my/UC-d5bbdc9f-62a2-4fe3-98ed-a8ac642f0880'
               >
                 NodeJS API With Express & MongoDB
               </a>

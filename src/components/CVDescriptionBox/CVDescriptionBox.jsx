7f1edde5f7159b4f48e4e1c095a46ffb3bd6ce1b
Gabriel Brun <lydstyl@gmail.com>
2021-01-27 15:31:13 +0100

    fix warnings

--- src/components/CVDescriptionBox/CVDescriptionBox.jsx
+++ src/components/CVDescriptionBox/CVDescriptionBox.jsx
@@ -36,9 +36,9 @@ const CVDescriptionBox = () => {
         width='560'
         height='315'
         src='https://www.youtube-nocookie.com/embed/u4LuxFWGtf0'
-        frameborder='0'
+        frameBorder='0'
         allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-        allowfullscreen
+        allowFullScreen
        />
     </div>
   )

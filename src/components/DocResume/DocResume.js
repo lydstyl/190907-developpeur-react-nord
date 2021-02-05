b3c286b9ef2509c60209814765371ede002b6a21
Gabriel Brun <lydstyl@gmail.com>
2021-02-05 21:19:17 +0100

    graphql query for DocResume working

--- src/components/DocResume/DocResume.js
+++ src/components/DocResume/DocResume.js
@@ -1,5 +1,5 @@
-// import React, { Component } from "react";
 import React from "react";
+import { useStaticQuery, graphql } from 'gatsby'
 import Docxtemplater from "docxtemplater";
 import PizZip from "pizzip";
 import PizZipUtils from "pizzip/utils/index.js";
@@ -9,69 +9,100 @@ function loadFile(url, callback) {
   PizZipUtils.getBinaryContent(url, callback);
 }
 
-export const DocResume = class App extends React.Component {
-
-  render() {
-
-    const {data} = this.props
-    console.log('🚀 ~ App ~ render ~ data', data)
-
-    const generateDocument = () => {
-      loadFile("http://localhost:8000/assets/resume-template.docx", function(
-        error,
-        content
-      ) {
-        if (error) {
-          throw error;
-        }
-        var zip = new PizZip(content);
-        var doc = new Docxtemplater().loadZip(zip);
-        doc.setData(data);
-        try {
-          // render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
-          doc.render();
-        } catch (error) {
-          // The error thrown here contains additional information when logged with JSON.stringify (it contains a properties object containing all suberrors).
-          function replaceErrors(key, value) {
-            if (value instanceof Error) {
-              return Object.getOwnPropertyNames(value).reduce(function(
-                error,
-                key
-              ) {
-                error[key] = value[key];
-                return error;
-              },
-              {});
+export const DocResume = () => {
+  const data = useStaticQuery(
+    graphql`
+      {
+        markdownRemark(fileAbsolutePath: { regex: "/cv/" }) {
+          frontmatter {
+            date(formatString: "DD/MM/YYYY")
+            description
+            objectif
+            skills {
+              main {
+                title
+                rate
+              }
+              goal {
+                title
+                rate
+              }
+              other {
+                title
+              }
             }
-            return value;
+            experience {
+              job
+              company
+              begin
+              ismycurrentjob
+              end
+            }
           }
-          console.log(JSON.stringify({ error: error }, replaceErrors));
-
-          if (error.properties && error.properties.errors instanceof Array) {
-            const errorMessages = error.properties.errors
-              .map(function(error) {
-                return error.properties.explanation;
-              })
-              .join("\n");
-            console.log("errorMessages", errorMessages);
-            // errorMessages is a humanly readable message looking like this :
-            // 'The tag beginning with "foobar" is unopened'
-          }
-          throw error;
         }
-        var out = doc.getZip().generate({
-          type: "blob",
-          mimeType:
-            "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-        }); //Output the document using Data-URI
-        saveAs(out, "output.docx");
-      });
-    };
+      }
+    `
+  )
 
-    return (
-      <div className="p-2">
+  const { frontmatter } = data.markdownRemark
+  console.log('🚀 ~ DocResume ~ frontmatter', frontmatter)
+
+
+  const generateDocument = () => {
+    loadFile("http://localhost:8000/assets/resume-template.docx", function(
+      error,
+      content
+    ) {
+      if (error) {
+        throw error;
+      }
+      var zip = new PizZip(content);
+      var doc = new Docxtemplater().loadZip(zip);
+      doc.setData(frontmatter);
+      try {
+        // render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
+        doc.render();
+      } catch (error) {
+        // The error thrown here contains additional information when logged with JSON.stringify (it contains a properties object containing all suberrors).
+        function replaceErrors(key, value) {
+          if (value instanceof Error) {
+            return Object.getOwnPropertyNames(value).reduce(function(
+              error,
+              key
+            ) {
+              error[key] = value[key];
+              return error;
+            },
+            {});
+          }
+          return value;
+        }
+        console.log(JSON.stringify({ error: error }, replaceErrors));
+
+        if (error.properties && error.properties.errors instanceof Array) {
+          const errorMessages = error.properties.errors
+            .map(function(error) {
+              return error.properties.explanation;
+            })
+            .join("\n");
+          console.log("errorMessages", errorMessages);
+          // errorMessages is a humanly readable message looking like this :
+          // 'The tag beginning with "foobar" is unopened'
+        }
+        throw error;
+      }
+      var out = doc.getZip().generate({
+        type: "blob",
+        mimeType:
+          "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+      }); //Output the document using Data-URI
+      saveAs(out, "output.docx");
+    });
+  };
+
+  return (
+    <div className="p-2">
         <button onClick={generateDocument}>Generate document</button>
       </div>
-    );
-  }
-};
+  )
+}

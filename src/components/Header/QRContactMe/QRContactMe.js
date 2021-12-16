c24dd3bd67b05b5e77ce572efbf93fd6419325a0
Gabriel Brun <lydstyl@gmail.com>
2021-12-17 00:17:04 +0100

    Change QR Code.

--- src/components/Header/QRContactMe/QRContactMe.js
+++ src/components/Header/QRContactMe/QRContactMe.js
@@ -1,19 +1,24 @@
 import React from "react"
 import styled from "styled-components"
+// import QRCode from "react-qr-code";
 
 const Img = styled.img`
   display: none;
 
   @media screen and (min-width: 768px) {
     display: block;
+    max-height: 144px;
+    margin: 0 10px;
   }
 `
 
 export const QRContactMe = () => {
+  // Generated with https://goqr.me/
+
   return (
     <Img
-      src="https://chart.googleapis.com/chart?cht=qr&chl=BEGIN%3AVCARD%0AVERSION%3A3.0%0AN%3ABrun%3BGabriel%0AFN%3AGabriel%20Brun%0AORG%3A%0ATITLE%3ADeveloppeur%20JS%20et%20React.js%0AADR%3A%3B%3B%3BRaismes%3B%3B59590%3BFrance%0ATEL%3BWORK%3BVOICE%3A%0ATEL%3BCELL%3A0781154503%0ATEL%3BFAX%3A%0AEMAIL%3BWORK%3BINTERNET%3Alydstyl%40gmail.com%0AURL%3Awww.developpeur-react-nord.com%0AEND%3AVCARD%0A&chs=180x180&choe=UTF-8&chld=L|2"
-      alt="vcard contactez moi"
+      src="http://api.qrserver.com/v1/create-qr-code/?color=000000&amp;bgcolor=FFFFFF&amp;data=BEGIN%3AVCARD%0AVERSION%3A2.1%0AFN%3AGabriel+Brun%0AN%3ABrun%3BGabriel%0ATEL%3BHOME%3BVOICE%3A0781154503%0AEMAIL%3BHOME%3BINTERNET%3Alydstyl%40gmail.com%0AURL%3Awww.developpeur-react-nord.com%0AADR%3A%3B%3B%3B%3B%3B%3BFrance%0AEND%3AVCARD%0A&amp;qzone=1&amp;margin=0&amp;size=400x400&amp;ecc=L"
+      alt="QR code Développeur React Nord"
       title="VCARD, mes coordonnées sont aussi en bas de page."
     />
   )
